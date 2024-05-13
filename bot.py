271784866675e095ad47d0c10153f28a9e3138e6
UNEMPLOID <141543574+UNEMPLOID@users.noreply.github.com>
2024-05-14 03:43:50 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,76 @@
+#(©)Codexbotz
+
+from aiohttp import web
+from plugins import web_server
+
+import pyromod.listen
+from pyrogram import Client
+from pyrogram.enums import ParseMode
+import sys
+from datetime import datetime
+
+from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID, PORT
+
+class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            name="File carry Bot",
+            api_hash="1994302e2d4319c92af3409c3c6dea91",
+            api_id="22247279",
+            plugins={
+                "root": "plugins"
+            },
+            workers=TG_BOT_WORKERS,
+            bot_token="7001109625:AAHYrgpKiwZcCHN6EWRDb7S3zJLSBG9Hl24",
+        )
+        self.LOGGER = LOGGER
+
+    async def start(self):
+        await super().start()
+        usr_bot_me = await self.get_me()
+        self.uptime = datetime.now()
+
+        if FORCE_SUB_CHANNEL:
+            try:
+                link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
+                if not link:
+                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL)
+                    link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
+                self.invitelink = link
+            except Exception as a:
+                self.LOGGER(__name__).warning(a)
+                self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
+                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/ii_hacker_ii for support")
+                sys.exit()
+        try:
+            db_channel = await self.get_chat(CHANNEL_ID)
+            self.db_channel = db_channel
+            test = await self.send_message(chat_id = , text = "Test Message")
+            await test.delete()
+        except Exception as e:
+            self.LOGGER(__name__).warning(e)
+            self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/ii_hacker_ii for support")
+            sys.exit()
+
+        self.set_parse_mode(ParseMode.HTML)
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/ii_Hacker_ii")
+        self.LOGGER(__name__).info(f""" \n\n
+______________________ ____________________   __
+___  ____/__    |__  / __  ____/_  __ \__  | / /
+__  /_   __  /| |_  /  _  /    _  / / /_   |/ /
+_  __/   _  ___ |  /___/ /___  / /_/ /_  /|  /
+/_/      /_/  |_/_____/\____/  \____/ /_/ |_/
+                                SECURITY....
+                                          """)
+        self.username = usr_bot_me.username
+        #web-response
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
+
+    async def stop(self, *args):
+        await super().stop()
+        self.LOGGER(__name__).info("Bot stopped.")
